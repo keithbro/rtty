865359f83c14361f2bbf3eb6c50ba61adffdcbf7
Keith Broughton <keithbro256@gmail.com>
2020-04-18 15:26:31 +1000

    WIP

--- ft8.py
+++ ft8.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyaudio
 import pprint
 import sys
+import re
 
 # np.set_printoptions(threshold=sys.maxsize)
 
@@ -38,47 +39,137 @@ output_stream = p.open(rate=SAMPLE_RATE,
 
 signal2 = np.ndarray(shape=(1,0), dtype=np.int16)
 
+CHUNK = int(SAMPLE_RATE/BAUD)
+
 def decode(data):
-  print("OK")
+  decoded = np.frombuffer(data, dtype=np.int16)
+  bins = np.fft.fft(decoded)
+  freqs = np.fft.fftfreq(len(bins))
+
+  x = []
+  for idx, bin in enumerate(bins):
+    freq = int(abs(freqs[idx] * SAMPLE_RATE))
+    amp = np.abs(bin)
+    x.append((freq, amp))
+
+  sorted_by_second = sorted(x, key=lambda tup: tup[1])
+
+  # print(sorted_by_second)
+  idx = np.argmax(np.abs(bins))
+  freq = freqs[idx]
+  freq_in_hertz = abs(freq * SAMPLE_RATE)
+  if freq_in_hertz > 950:
+    return "1"
+  else:
+    return "0"
+  
 
 # 8000 / 1024 * 1 = 7.8 = 7
 # 8000 samples / 45.45 BAUD * SECONDS * 1 = 176 reads
 
+res = []
+
 for i in range(0, int(SAMPLE_RATE / BAUD * SECONDS)):
     data = input_stream.read(CHUNK)
-    frequencies = decode(data)
-    print(len(data))
-    #decoded = np.frombuffer(data, dtype=np.int16)
-    #signal2 = np.append(signal2, decoded)
+    res.append(decode(data))
+    decoded = np.frombuffer(data, dtype=np.int16)
+    signal2 = np.append(signal2, decoded)
 
-exit()
+print("".join(res))
+
+# https://www.dcode.fr/baudot-code
+baudot = {
+  "L": {
+    "10000": "A",
+    "00110": "B",
+    "01110": "C", #
+    "01000": "E",
+    "01101": "F", #
+    "11010": "G", #
+    "10100": "H",
+    "01111": "K", #
+    "10010": "L",
+    "01011": "M",
+    "01100": "N", #
+    "11100": "O",
+    "10110": "P", #
+    "10111": "Q",
+    "01010": "R", #
+    "00101": "S",
+    "10101": "T",
+    "00111": "U", #
+    "11110": "V",
+    "": "W",
+    "01001": "X",
+    "00100": "Y",
+    "11001": "Z",
+    "11000": "/",
+    "10001": "-",
+    "00010": "FS",
+    "11111": " ",
+  },
+  "F": {
+    "00010": " ",
+    "11010": "&",
+    "10100": "#",
+    "11100": ".",
+    "11110": ";",
+    "10110": "0",
+    "01010": "4",
+    "00111": "7",
+    "11000": "9",
+    "11111": "LS",
+  }
+}
+
+baudot2 = {
+  "L": {
+    "11000": "A",
+    "10011": "B",
+    "10110": "F",
+    "01001": "L",
+    "11100": "U",
+    "01111": "V",
+    "10001": "Z",
+  }
+}
+
+mode = "L"
+pos = 0
+lock = False
+
+def fs(mode):
+  if mode == "L":
+    return "F"
+  elif mode == "F":
+    return "L"
+
+while pos < len(res):
+  c = res[pos:pos+5]
+  bit_str = "".join(c)
+  x = re.match(r'^0.+11$', bit_str)
+
+  if x is None:
+    pos += 1
+    next
+
+  lock = True
+  pos += 5
+
+  try:
+    decoded = baudot[mode][bit_str]
+  except:
+    print("ERROR")
+    print("mode: " + mode)
+    print("binary: " + bit_str)
+
+  if decoded == "FS":
+    mode = "F"
+  elif decoded == "LS":
+    mode = "L"
+  else:
+    print(decoded)
 
 input_stream.stop_stream()
 input_stream.close()
-
-print(signal2)
 output_stream.write(signal2)
-
-bins = np.fft.fft(signal2)
-# print(bins)
-print(len(bins))
-
-freqs = np.fft.fftfreq(len(bins))
-# print(freqs)
-
-x = []
-for idx, bin in enumerate(bins):
-  freq = int(abs(freqs[idx] * SAMPLE_RATE))
-  amp = np.abs(bin)
-  x.append((freq, amp))
-
-sorted_by_second = sorted(x, key=lambda tup: tup[1])
-
-print(sorted_by_second)
-
-idx = np.argmax(np.abs(bins))
-freq = freqs[idx]
-print(freq)
-
-freq_in_hertz = abs(freq * SAMPLE_RATE)
-print(freq_in_hertz)
